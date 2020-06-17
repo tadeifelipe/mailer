c92ab7152354c93678b97eef39e99ed8c176a4db
tadeifelipe <felipe_santostadei@hotmail.com>
2020-06-17 00:06:13 -0300

    change default

--- app/src/main/java/com/example/mailer/MainActivity.java
+++ app/src/main/java/com/example/mailer/MainActivity.java
@@ -4,36 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("App Mailer");
-        final String email = findViewById(R.id.activity_main_email).toString();
-        final String corpo = findViewById(R.id.activity_main_corpo).toString();
 
         Button buttonSend = findViewById(R.id.activity_main_botao_enviar);
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendEmail(MainActivity.this, email, corpo);
+                sendEmail(MainActivity.this);
             }
         });
 
     }
 
-    public void sendEmail(Context context, String email, String corpo) {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_EMAIL,email);
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Teste");
-        intent.putExtra(Intent.EXTRA_TEXT, corpo);
-        context.startActivity(Intent.createChooser(intent,"Choose your manager E-mail:"));
+    public void sendEmail(Context context) {
+        EditText recipient = findViewById(R.id.activity_main_email);
+        EditText corpo = findViewById(R.id.activity_main_corpo);
+        EditText subject = findViewById(R.id.activity_main_subject);
+        String[] recipientString = recipient.getText().toString().split(",");
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL,recipientString);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+        intent.putExtra(Intent.EXTRA_TEXT, corpo.getText().toString());
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent,"Choose your manager E-mail:"));
     }
 }
